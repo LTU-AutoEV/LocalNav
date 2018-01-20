ac3965be6c67771febbab86cc8e551c6a69985ea
charles <charleslhf@gmail.com>
2018-01-20 13:21:38 -0500

    Created skeleton for converter file

--- src/pointcloud2gridmap.cpp
+++ src/pointcloud2gridmap.cpp
@@ -1,9 +1,31 @@
 #include <ros/ros.h>
 #include <iostream>
 
+#include <sensor_msgs/PointCloud2.h>
+#include "grid_map_core/GridMap.hpp"
+
+class PCloud2GMap{
+public:
+PCloud2GMap();
+
+private:
+void CloudCallback(const sensor_msgs::PointCloud2& cloud); 
+
+ros::NodeHandle nh_;
+ros::Publisher pub_:
+ros::Subscriber sub_;
+};
+
+PCloud2GMap::PCloud2GMap(){
+
+}
+
+void PCloud2GMap::CloudCallback(const sensor_msgs::PointCloud2& cloud){
+
+} 
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
 
-    return 0;
+
 }
