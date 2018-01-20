c024338a681dd0a773d52d2bb46e8162a2e62b61
Nicholas Paul <npaul@ltu.edu>
2018-01-20 11:37:50 -0500

    add src files

---
+++ src/gridmap2costmap.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
+
---
+++ src/pointclout2gridmap.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
