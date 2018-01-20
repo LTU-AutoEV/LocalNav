7771af73c16e65d90e3db560ee4ad3bcd4122169
charles <charleslhf@gmail.com>
2018-01-20 14:32:34 -0500

    Finished cloud2map constructor

--- src/pointcloud2gridmap.cpp
+++ src/pointcloud2gridmap.cpp
@@ -2,30 +2,36 @@
 #include <iostream>
 
 #include <sensor_msgs/PointCloud2.h>
-#include "grid_map_core/GridMap.hpp"
+#include <grid_map_msgs/GridMap.h>
 
 class PCloud2GMap{
+
 public:
-PCloud2GMap();
+    PCloud2GMap();
 
 private:
-void CloudCallback(const sensor_msgs::PointCloud2& cloud); 
+    void CloudCallback(const sensor_msgs::PointCloud2& cloud); 
 
-ros::NodeHandle nh_;
-ros::Publisher pub_:
-ros::Subscriber sub_;
+    ros::NodeHandle nh_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
 };
 
 PCloud2GMap::PCloud2GMap(){
+    nh_ = ros::NodeHandle("~");
 
+    pub_ = nh_.advertise<grid_map_msgs::GridMap>("grid_map", 100);
+    sub_ = nh_.subscribe("/velodyne_points", 100, &PCloud2GMap::CloudCallback, this);
 }
 
 void PCloud2GMap::CloudCallback(const sensor_msgs::PointCloud2& cloud){
 
 } 
 
-int main()
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "PointCloud2GridMap");
+  PCloud2GMap PCloud2GMap;
 
-
+  ros::spin();
 }
