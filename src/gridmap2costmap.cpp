a417fab286d8971c142bc5278288804943741cbc
Nicholas Paul <npaul@ltu.edu>
2018-01-20 19:29:54 -0500

    gridmap2costmap: publish occupancy grid

--- src/gridmap2costmap.cpp
+++ src/gridmap2costmap.cpp
@@ -1,12 +1,63 @@
 #include <ros/ros.h>
 #include "grid_map_core/GridMap.hpp"
+#include "grid_map_costmap_2d/grid_map_costmap_2d.hpp"
+#include "grid_map_msgs/GridMap.h"
+#include "grid_map_ros/grid_map_ros.hpp"
+
+#include <nav_msgs/OccupancyGrid.h>
 
 #include <iostream>
 
-int main()
+/* GridMap2CostMap
+ *
+ * Convert grid map ro cost map
+ *
+ */
+class GridMap2CostMap
 {
-    std::cout << "Hello world" << std::endl;
+public:
+    GridMap2CostMap();
 
-    return 0;
+private:
+    // Callbacks
+    void gmCallback(const grid_map_msgs::GridMap& scan);
+
+    // ROS objects
+    ros::NodeHandle nh_;
+    ros::Subscriber gm_sub_;
+    tf::TransformListener tf_;
+    ros::Publisher pub_;
+};
+
+// Constructor
+//   Set up publisher and subscriber
+GridMap2CostMap::GridMap2CostMap()
+    :nh_("~")
+{
+
+    gm_sub_  = nh_.subscribe("grid_map", 10, &GridMap2CostMap::gmCallback, this);
+    pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("occupancy_grid", 10);
+}
+
+// Callback function for the points
+void GridMap2CostMap::gmCallback(const grid_map_msgs::GridMap& msg)
+{
+    grid_map::GridMap gm;
+    grid_map::GridMapRosConverter::fromMessage(msg, gm);
+
+    // Convert to OccupancyGrid msg.
+    nav_msgs::OccupancyGrid occupancyGrid;
+    grid_map::GridMapRosConverter::toOccupancyGrid(gm, "layer", 0.0, 1.0, occupancyGrid);
+
+    pub_.publish(occupancyGrid);
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "gridmap2costmap");
+  GridMap2CostMap GridMap2CostMap;
+
+  ros::spin();
 }
 
