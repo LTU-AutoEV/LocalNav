b43a650a24ae7d42d165d02135d8de5b1f376fe7
CLHF <charleslhf@gmail.com>
2018-05-04 13:27:14 -0400

    moved trasform into its own function

--- src/local_nav/src/pointcloud2Occupancy.cpp
+++ src/local_nav/src/pointcloud2Occupancy.cpp
@@ -13,16 +13,15 @@
 #include <math.h>
 //#include <pcl/PCLPointCloud2.h>
 
-const int hieght = 3;
-const int length = 4;
-const int width = 3;
+using actor_cloud_t = pcl::PointCloud<pcl::PointXYZ>;
+using actor_cloud_ptr_t = actor_cloud_t::Ptr;
 
 /*PCloud2GMap
  *
  * Converts the PointCloud2 data from the velodyne Lidar
  *  into a grid_map
  *
- * Publish:     grid_map on "~/grid_map"
+ * Publish:     grid_map on "/grid_map"
  *
  * Subscrivbe:  PointCloud2 on "/velodyne_points"
  */
@@ -32,7 +31,7 @@ public:
     PCloud2GMap();
 
 private:
-
+    void TransformToBase(actor_cloud_ptr_t& out, actor_cloud_ptr_t& in);
     bool WithinThreshold(const float& x, const float& y, const float& z );
 
     // Callback
@@ -42,7 +41,7 @@ private:
     ros::Publisher  pub_;
     ros::Subscriber sub_;
 
-    float car_z_threshold; //the roof of the car
+    float z_max; //the roof of the car
     float x_min;
     float x_max;
     float width; 
@@ -52,12 +51,14 @@ private:
 //  set up publisher and subscriber
 PCloud2GMap::PCloud2GMap()
 {
-    //anthing too close to the lidar needs
-    // to be removed because we are seeing the car
-    car_z_threshold = 1.0f;
-    x_min = 0.5f;
-    x_max = 2.5f;
-    width = 2.0f; 
+    //We only care about points within 
+    // Z E {0,..,z_max}
+    // X E {x_min,...,x_max}
+    // Y E {-width/2,...,width/2}
+    z_max = 2.0f; //TODO add to dynamic reconfigure
+    x_min = 1.0f; //TODO add to dynamic reconfigure
+    x_max = 5.0f; //TODO add to dynamic reconfigure
+    width = 2.0f; //TODO add to dynamic reconfigure 
 
     nh_ = ros::NodeHandle("~");
     // publish on ~/obstacle_loc
@@ -70,7 +71,7 @@ PCloud2GMap::PCloud2GMap()
 bool PCloud2GMap::WithinThreshold(const float& x, const float& y, const float& z ){
 
  
-    if (z < car_z_threshold){
+    if (z < z_max){
         if (x >= x_min && x <= x_max){
             if (y > -(width/2) && y < (width/2)){
                 return true;
@@ -93,16 +94,49 @@ void PCloud2GMap::CloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 
 
     pcl::PCLPointCloud2 pcl_pc2;
+    //convert from msg to pcl
     pcl_conversions::toPCL(*msg,pcl_pc2);
 
     //create input cloud
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-    //create rotated cloud 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr centeredCloud(new pcl::PointCloud<pcl::PointXYZ>);
+    actor_cloud_ptr_t cloud(new pcl::PointCloud<pcl::PointXYZ>);
     
+    //convert from pcl to ros
     pcl::fromPCLPointCloud2(pcl_pc2,*cloud);
 
+    //create cloud for transforming
+    actor_cloud_ptr_t centeredCloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    //The ground plane and rotation may not be needed.
+    //It seems like the puck is able to do it for us.    
+    TransformToBase(cloud,centeredCloud);
+
+    geometry_msgs::PointStamped closest_point;
+
+    closest_point.header = msg->header;
+    closest_point.point.x = INT_MAX;
+
+    double dist = INT_MAX;
+
+    for (auto p : *cloud){
+        if (WithinThreshold(p.x, p.y, p.z)){
+            double dist2 =  sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
+            if (dist > dist2){
+                dist = dist2;
+                closest_point.point.x = p.x;
+                closest_point.point.y = p.y;
+                closest_point.point.z = p.z;
+            }
+        }
+    }
+
+    ROS_DEBUG("Point: %f", dist);
+
+    pub_.publish(closest_point);
+}
+
+
+void PCloud2GMap::TransformToBase(actor_cloud_ptr_t& out, actor_cloud_ptr_t& in)
+{
     //find ground plane (http://pointclouds.org/documentation/tutorials/planar_segmentation.php)
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
@@ -116,7 +150,7 @@ void PCloud2GMap::CloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
     seg.setMethodType (pcl::SAC_RANSAC);
     seg.setDistanceThreshold (0.01);
 
-    seg.setInputCloud (cloud);
+    seg.setInputCloud (in);
     seg.segment(*inliers, *coefficients);
 
     //(https://stackoverflow.com/questions/32299903/rotation-and-transformation-of-a-plane-to-xy-plane-and-origin-in-pointcloud)
@@ -141,30 +175,8 @@ void PCloud2GMap::CloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
     Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
     transform_2.rotate (Eigen::AngleAxisf (theta, rotation_vector));
     transform_2.translation() << 0, 0, 30; //is 30 correct?
-    pcl::transformPointCloud (*cloud, *centeredCloud, transform_2);
+    pcl::transformPointCloud (*in, *out, transform_2);
 
-    geometry_msgs::PointStamped closest_point;
-
-    closest_point.header = msg->header;
-    closest_point.point.x = INT_MAX;
-
-    double dist = INT_MAX;
-
-    for (auto p : *cloud){
-        if (WithinThreshold(p.x, p.y, p.z)){
-            double dist2 =  sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
-            if (dist > dist2){
-                dist = dist2;
-                closest_point.point.x = p.x;
-                closest_point.point.y = p.y;
-                closest_point.point.z = p.z;
-            }
-        }
-    }
-
-    ROS_DEBUG("Point: %f", dist);
-
-    pub_.publish(closest_point);
 }
 
 int
